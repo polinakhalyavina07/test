df7595a64e25af9dcf41fa395cf4aa6b8ed8b1e4
polinakhalyavina <polinakhalyavina07@yandex.ru>
2022-07-30 21:30:10 +0500

    first commit

---
+++ Main.py
@@ -0,0 +1,10 @@
+from Person import Person
+
+bob = Person("Bob", 26, "Volgograd")
+anna = Person("Anna", 22, "Moscow")
+petya = Person("Petya", 18, "New Lialia")
+masha = Person("Masha", 17, "Sankt - Peterburg")
+
+petya.age = -120
+
+print(petya.age)
---
+++ Person.py
@@ -0,0 +1,6 @@
+class Person:
+    def __init__(self, name, age, city):
+        self.name = name
+        self.__age = age
+        self.city = city
+
